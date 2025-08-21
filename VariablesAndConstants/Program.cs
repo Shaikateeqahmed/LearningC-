1dad03bd0ae20a090f66a8067903399021eee6bf
Shaik Ateeq Ahmed <sahmed@ariqt.com>
2025-08-21 16:48:37 +0530

    Learn variables and constant in C#

---
+++ VariablesAndConstants/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//all the key words is in blue color and starting with small letter
+//byte number = 5;
+//int count = 10;
+//float totalPrice = 20.25f;
+//char character = 'A';
+//string firstName = "Shaik";
+//bool isWorking = true;
+
+var number = 5;
+var count = 10;
+var totalPrice = 20.25f;
+var character = 'A';
+var firstName = "Shaik";
+var isWorking = true;
+Console.WriteLine(number);
+Console.WriteLine(count);
+Console.WriteLine(totalPrice);
+Console.WriteLine(character);
+Console.WriteLine(firstName);
+Console.WriteLine(isWorking);
+
+Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
