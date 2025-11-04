fad5245cdd6c6d6ad7614181881dc2b349841edc
Shaik Ateeq Ahmed <sahmed@ariqt.com>
2025-11-04 14:08:26 +0530

    learn operators in C#

---
+++ Operators/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+//floating result
+//var a = 10;
+//var b = 3;
+
+//Console.WriteLine(a / b); //output 3
+//Console.WriteLine((float)a / (float)b); //output 3.33333 because its doing cast for type conversion
+
+
+//operator precedence
+var a = 1;
+var b = 2;
+var c = 3;
+
+Console.WriteLine(a + b * c); //* and / operators have high precedence then + and -.  output 7
+Console.WriteLine((a + b) * c);// if we want to change the precedence, then we can use (). output 9
