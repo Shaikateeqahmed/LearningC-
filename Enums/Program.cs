d3f46f5734612c1bb74fce07c9d2666df40905ef
Shaik Ateeq Ahmed <sahmed@ariqt.com>
2025-11-07 12:19:56 +0530

    Learned Enums and casting int or string to enum and parse the string into enum type

---
+++ Enums/Program.cs
@@ -0,0 +1,30 @@
+﻿var method = ShippingMethod.Express;
+Console.WriteLine(method);
+Console.WriteLine(method.ToString());
+
+
+//converting key to value of enum
+Console.WriteLine((int)method);
+
+
+//converting value to key of enum 
+var methodId = 3;
+Console.WriteLine((ShippingMethod)methodId);
+
+//convert or parse a string to an enum
+var methodName = "Express";
+
+var shippingMethod = Enum.Parse(typeof(ShippingMethod), methodName);
+
+Console.WriteLine(shippingMethod);
+
+Console.WriteLine(shippingMethod.GetType());
+public enum ShippingMethod
+{
+    RegularAirMail = 1,
+    RegisteredAirMail = 2,
+    Express = 3,
+}
+
+
+
