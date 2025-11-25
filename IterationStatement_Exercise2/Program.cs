c6b92399457e461859135e15bf4d824ce45a011f
Shaik Ateeq Ahmed <sahmed@ariqt.com>
2025-11-25 11:45:56 +0530

    cover exersice 2 of Iteration statement

---
+++ IterationStatement_Exercise2/Program.cs
@@ -0,0 +1,26 @@
+﻿
+//Q2- Write a program and continuously ask the user to enter a number or "ok" to exit. Calculate the sum of all the previously entered numbers and display it on the console.
+
+int sum = 0;
+
+while (true)
+{
+    Console.Write("Enter a number or \"ok\" to exit: ");
+    string input = Console.ReadLine();
+
+    if (input.ToLower() == "ok")
+        break;
+
+    int number;
+
+    if (int.TryParse(input, out number))
+    {
+        sum += number;
+    }
+    else
+    {
+        Console.WriteLine("Invalid input. Please enter a valid number.");
+    }
+}
+
+Console.WriteLine("Total Sum = " + sum);
