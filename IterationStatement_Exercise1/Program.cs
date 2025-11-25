63f9e28f594ee206408ee7a34e565d3b73caeeb1
Shaik Ateeq Ahmed <sahmed@ariqt.com>
2025-11-25 11:32:21 +0530

    cover exersice 1 of iteration statement

---
+++ IterationStatement_Exercise1/Program.cs
@@ -0,0 +1,10 @@
+﻿//@1- Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder. Display the count on the console.
+
+var count = 0;
+
+for (var i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0) count++;
+}
+
+Console.WriteLine(count);
