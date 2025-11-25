b64402d619fc24f5b46aa7b721ba3c9388d74ead
Shaik Ateeq Ahmed <sahmed@ariqt.com>
2025-11-25 11:54:13 +0530

    cover exercise 3 of iteration statement

---
+++ IterationStatement_Exercise3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Q3- Write a program and ask the user to enter a number. Compute the factorial of the number and print it on the console. For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.
+
+Console.WriteLine("Enter a number, I will calculate the factorial of it! :");
+
+var number = int.Parse(Console.ReadLine());
+
+var factorial = 1;
+
+for (int i = 1; i <= number; i++)
+{
+    factorial *= i;
+}
+
+Console.WriteLine($"{number}! = {factorial}");
