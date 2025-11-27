e2c8f5233a6ba6b0060b72879265b026e2bce57b
Shaik Ateeq Ahmed <sahmed@ariqt.com>
2025-11-27 12:02:27 +0530

    solved arrays exercise 2

---
+++ Arrays_Exercise2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Q2- Write a program and ask the user to enter their name. Use an array to reverse the name and then store the result in a new string. Display the reversed name on the console.
+
+Console.WriteLine("Enter Your Name: ");
+var userName = Console.ReadLine().ToCharArray();
+
+Array.Reverse(userName);
+
+var reverseNameStr = new string(userName);
+
+Console.WriteLine(reverseNameStr);
+
