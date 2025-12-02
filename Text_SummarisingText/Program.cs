cd00b60ae8ea5853c834ddba735316c46d115e82
Shaik Ateeq Ahmed <sahmed@ariqt.com>
2025-12-02 14:20:30 +0530

    write the programe for summarising the tex

---
+++ Text_SummarisingText/Program.cs
@@ -0,0 +1,8 @@
+﻿//Summarising Text
+
+using Text_SummarisingText;
+
+var sentence = "this is going to be a really really really long text";
+StringUtility.SummerizeText(sentence);
+Console.WriteLine(StringUtility.SummerizeText(sentence));
+
---
+++ Text_SummarisingText/StringUtility.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_SummarisingText
+{
+    public class StringUtility
+    {
+        public static string SummerizeText(string sentence)
+        {
+            var maxLength = 20;
+            var summaryList = new List<string>();
+
+            if (sentence.Length < maxLength)
+            {
+                return sentence;
+            }
+            else
+            {
+                var words = sentence.Split(' ');
+
+                var totalWordsLenght = 0;
+
+                foreach (var word in words)
+                {
+                    summaryList.Add(word);
+                    totalWordsLenght += word.Length + 1;
+                    if (totalWordsLenght > maxLength) break;
+                }
+
+                return String.Join(" ", summaryList) + "...";
+
+            }
+        }
+    }
+}
