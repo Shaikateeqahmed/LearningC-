6ecc9a08dc0cb48ada48b9181a7a96607285bc04
Shaik Ateeq Ahmed <sahmed@ariqt.com>
2025-12-03 12:31:52 +0530

    Learned String Builders and its methods

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Text;
+
+var builder = new StringBuilder();
+
+//we can also start with defauld result
+//var builder = new StringBuilder("Hello world");
+
+//append methods
+//builder.Append('-', 10);
+//builder.AppendLine();
+//builder.Append("Header");
+//builder.AppendLine();
+//builder.Append('-', 10);
+
+////Replace
+//builder.Replace('-', '+');
+
+////Remove 
+//builder.Remove(0, 10);
+
+//insert
+//builder.Insert(0, new string('-', 10));
+
+//chaining the stringBuilders
+builder.Append('-', 10)
+.AppendLine()
+.Append("Header")
+.AppendLine()
+.Append('-', 10)
+
+//Replace
+.Replace('-', '+')
+
+//Remove 
+.Remove(0, 10)
+
+//insert
+.Insert(0, new string('-', 10));
+
+Console.WriteLine(builder);
+Console.WriteLine("1st char : "+builder[0]);
