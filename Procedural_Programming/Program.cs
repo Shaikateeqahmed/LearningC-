aa201a70c09c19a004d9e54cd4c1d488ce349a9e
Shaik Ateeq Ahmed <sahmed@ariqt.com>
2025-12-04 15:53:44 +0530

    cover procedural programming

---
+++ Procedural_Programming/Program.cs
@@ -0,0 +1,23 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("What's your Name?");
+        var name = Console.ReadLine();
+
+        var reversed = ReverseName(name);
+
+        Console.WriteLine("Reversed Name : " + reversed);
+    }
+
+    public static string ReverseName(string name)
+    {
+        var array = new char[name.Length];
+        for (int i = name.Length; i > 0; i--)
+        {
+            array[name.Length - i] = name[i - 1];
+        }
+        return new string(array);
+    }
+}
